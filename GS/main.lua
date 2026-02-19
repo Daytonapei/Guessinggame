bd2a552c4a762ce29bfde692f88ecbda0c496182
Daytonapei <muffbai@gmail.com>
2026-02-19 00:14:14 -0600

    Update main.lua

--- GS/main.lua
+++ GS/main.lua
@@ -1,5 +1,17 @@
 local userContinue = "yes"
 
+math.randomseed(os.time())
 
-print("pick a number between 1 and 100")
-print("you have 8 attempt to guess it")
+while userContinue == "yes" do
+    local computerNumber = math.random(1,100)
+    local attempt = 7
+
+    print("pick a number between 1 and 100")
+    print("you have 8 attempt to guess it")
+
+    while attempt > 0 do
+        print("Enter your guess:")
+        local input = io.read()
+        local guess = tonumber(input)
+    end
+end
